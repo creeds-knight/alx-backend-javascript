c270083bf0a453f4b238b0a4907a297c9d596fb5
creeds-knight <apedoarthur21@gmail.com>
2024-06-28 23:17:55 +0300

    task12, report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(lst) {
+      return Object.keys(lst).length;
+    },
+  };
+}
