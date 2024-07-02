57030a0ecf9db9c35b8ed8782d4ed8e608937c11
creeds-knight <apedoarthur21@gmail.com>
2024-07-02 12:51:53 +0300

    task2, filter

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(arr, city) {
+  if (!Array.isArray(arr) || typeof city !== 'string') {
+    return [];
+  }
+  return arr.filter((std) => std.location === city);
+}
