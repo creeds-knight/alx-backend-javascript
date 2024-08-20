7a9defc68f32bf054b51797e78eb129a57bc931e
creeds-knight <apedoarthur21@gmail.com>
2024-08-20 12:14:06 +0300

    task1, process.stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,6 @@
 // Using process stdin
-console.log('Welcome to Holberton School, what is your name?');
-// process.stdin.setEnconding("utf-8")
-process.stdin.on('readable', function () {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
-  }
-});
+console.log("Welcome to Holberton School, what is your name?")
+//process.stdin.setEnconding("utf-8")
+process.stdin.on("readable", function(chunk){
+    console.log("Your name is: " + chunk.trim());
+})
